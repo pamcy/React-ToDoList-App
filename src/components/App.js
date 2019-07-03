7429721b0d378043b7d3902ea4b5a6130a363844
Pamcy <chinyi.pam@gmail.com>
2019-07-03 18:22:54 +0800

    :sparkles: Add Github Corner

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import TabMenuList from './TabMenuList';
 import TabContent from './TabContent';
+import GithubBanner from './GithubBanner';
 
 const App = () => {
   const routes = [
@@ -23,6 +24,7 @@ const App = () => {
   return (
     <Router>
       <div className="container">
+        <GithubBanner />
         <TabMenuList />
         {routes.map((route, index) => (
           <Route key={index} path={route.path} exact={route.exact} component={route.component} />
---
+++ src/components/GithubBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GithubCorner from 'react-github-corner';
+
+const GithubBanner = () => (
+  <div className="github-banner">
+    <GithubCorner
+      href="https://github.com/pamcy/React-ToDoList-App"
+      bannerColor="#283044"
+      size="77"
+      target="_blank"
+    />
+  </div>
+);
+
+export default GithubBanner;
