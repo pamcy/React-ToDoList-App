f5fa3830fa34b1d354483a71e82c69e9613d26aa
Pamcy <chinyi.pam@gmail.com>
2019-07-04 16:14:43 +0800

    :package: Add build folder

---
+++ build/precache-manifest.ff20966473cb7a8e94a85342b62493d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440a6526a7ee08c5d1d3d49199a1d5bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed1990387541e38907f7",
+    "url": "/static/css/main.f7ff2fec.chunk.css"
+  },
+  {
+    "revision": "85b351c26fca58c804ee",
+    "url": "/static/js/2.491c378c.chunk.js"
+  },
+  {
+    "revision": "ed1990387541e38907f7",
+    "url": "/static/js/main.a0848fe7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
